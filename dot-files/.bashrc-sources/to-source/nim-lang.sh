97f281ccf9ae3ef109573e65149f01532814a3d1
Alexander Kosnac <alexander.kosnac@web.de>
2021-05-15 16:42:20 +0200

    Choosenim sourcable

---
+++ dot-files/.bashrc-sources/to-source/nim-lang.sh
@@ -0,0 +1,7 @@
+FILE="${HOME}/.cargo/env"
+# Repository: https://github.com/dom96/choosenim
+# Install via:
+#     curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+
+export PATH=${HOME}/.nimble/bin:$PATH
+
