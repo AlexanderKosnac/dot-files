41198042040e722260222bdd620704a155c745ce
Alexander Kosnac <alexander.kosnac@web.de>
2021-09-01 22:47:17 +0200

    Makefile autocomplete

---
+++ dot-files/.bashrc-sources/to-source/makefile-autocomplete.sh
@@ -0,0 +1,5 @@
+# Automatically parses a makefile and provides autocompletion.
+# https://stackoverflow.com/a/38415982/6921511
+
+complete -W "\`grep -oE '^[a-zA-Z0-9_.-]+:([^=]|$)' ?akefile | sed 's/[^a-zA-Z0-9_.-]*$//'\`" make
+
