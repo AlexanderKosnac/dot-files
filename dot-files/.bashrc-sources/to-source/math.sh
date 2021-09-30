c73a6a463c8de45a5c68e2880712435d9f417a2e
Alexander Kosnac <alexander.kosnac@web.de>
2021-09-30 14:23:05 +0200

    Extract math functions into sourceable

---
+++ dot-files/.bashrc-sources/to-source/math.sh
@@ -0,0 +1,14 @@
+bytes-to-human() {
+    b=${1:-0}; d=''; s=0; S=(Bytes {K,M,G,T,P,E,Z,Y}iB)
+    while ((b > 1024)); do
+        d="$(printf ".%02d" $((b % 1024 * 100 / 1024)))"
+        b=$((b / 1024))
+        let s++
+    done
+    echo "$b$d ${S[$s]}"
+}
+
+get-percentage() {
+    $python_3_8 -c "print(\"{0:.0%}\".format($1/$2))"
+}
+
