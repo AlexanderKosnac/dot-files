fadad1f7cb756927a2e488ca28da526db2baf9ba
Alexander Kosnac <alexander.kosnac@web.de>
2021-09-30 18:09:39 +0200

    Rework prompt to be more flexible

--- dot-files/.bashrc-sources/to-source/prompt.sh
+++ dot-files/.bashrc-sources/to-source/prompt.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-def="$(color-rgb-fg 250 250 250 40 40 40)"
+def="${S_RESET}"
 blue="$(color-rgb-fg 0 133 251)"
 yellow="$(color-rgb-fg 255 208 0)"
+red="$(color-rgb-fg 255 110 110)"
 green="$(color-rgb-fg 89 255 114)"
 gray="$(color-rgb-fg 200 200 200)"
 
@@ -13,6 +14,30 @@ p_time="${gray}\t${def}"
 p_colon="${def}:${def}"
 p_pwd="${blue}\W${def}"
 
-prompt="${p_user}${p_at}${p_host} ${p_time}: ${p_pwd}${def}"
-export PROMPT_COMMAND='__git_ps1 "${prompt}" "\n$ "'
+function __setprompt
+{
+    ec=$?;  # has to be at top
+    PS1=""  # clear PS1
 
+    # user and host
+    PS1+="${p_user}${p_at}${p_host} "
+
+    # timestamp
+    PS1+="${p_time}"
+
+    PS1+="${p_colon} "
+    PS1+="${p_pwd}"
+    PS1+="${def}"
+    PS1+="\n"
+    PS1+="\$"
+
+	# PS2 for continuing multiline commands
+	#PS2=""
+
+	# PS3 for entering a number choice in scripts
+	#PS3=""
+
+	# PS4 for tracing in debug mode
+	#PS4=""
+}
+PROMPT_COMMAND='__setprompt'
