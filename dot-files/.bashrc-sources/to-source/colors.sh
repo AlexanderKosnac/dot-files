262b0b5f7823f0289efb7500ddb680e1c308385c
Alexander Kosnac <alexander.kosnac@web.de>
2021-09-30 17:26:42 +0200

    Add font styles to colors

--- dot-files/.bashrc-sources/to-source/colors.sh
+++ dot-files/.bashrc-sources/to-source/colors.sh
@@ -3,7 +3,31 @@
 # the most perfect stackoverflow answer on the topic of colors:
 # https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
 
-# font
+# font style
+export S_RESET="\033[0m"
+export S_BOLD="\033[1m"
+export S_BOLD_N="\033[22m"
+
+export S_FAINT="\033[2m"
+export S_FAINT_N="\033[22m"
+
+export S_ITALIC="\033[3m"
+export S_ITALIC_N="\033[23m"
+
+export S_UNDERLINE="\033[4m"
+export S_UNDERLINE_N="\033[24m"
+
+export S_BLINK_SLOW="\033[5m"
+export S_BLINK_RAPID="\033[6m"
+export S_BLINK_N="\033[25m"
+
+export S_CROSSED="\033[9m"
+export S_CROSSED_N="\033[29m"
+
+export S_OVERLINED="\033[53m"
+export S_OVERLINED_N="\033[53m"
+
+# color
 export C_NC='\e[0m' # No Color
 export C_BLACK='\e[0;30m'
 export C_GRAY='\e[1;30m'
@@ -21,6 +45,9 @@ export C_CYAN='\e[0;36m'
 export C_LIGHT_CYAN='\e[1;36m'
 export C_LIGHT_GRAY='\e[0;37m'
 export C_WHITE='\e[1;37m'
+
+export DEF_FG="\033[39m"
+export DEF_BG="\033[49m"
 
 # parameters are layer (fg=38 ; bg=48) and r, g, b values in this order (0-255)
 function _color-rgb() {
@@ -53,4 +80,3 @@ function all-colors() {
         done
     done
 }
-
