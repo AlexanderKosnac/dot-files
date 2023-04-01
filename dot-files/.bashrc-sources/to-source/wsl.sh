3bdf6d8a3eee67a82f63b5c4fb5c6b63c4f65069
Alexander Kosnac <alexander.kosnac@web.de>
2023-04-01 13:19:57 +0200

    WSL exclusive sources

---
+++ dot-files/.bashrc-sources/to-source/wsl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Only applied when used in Windows Subsystem for Linux (WSL)
+
+# check if we are inside WSL
+grep -iq "microsoft" /proc/version
+if [[ $! -ne 0 ]]; then
+    return
+fi
+
+# The native git in WSL is very slow while working on a Windows mount. Instead
+# use git.exe, when operating on the C: mount.
+# Taken from: https://markentier.tech/posts/2020/10/faster-git-under-wsl2/
+function git() {
+  if $(pwd -P | grep -q "^\/mnt\/c\/*"); then
+    git.exe "$@"
+  else
+    command git "$@"
+  fi
+}
